1e18fa7459f9e2010790b886fc067f9c3a62273c
sarah mamdouh <sarah.m.alawady@gmail.com>
2023-10-03 03:52:07 -0700

    comb3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for a in range(9):
+    for b in range(a + 1, 10):
+        if a < 8:
+            print("{:d}{:d}".format(a, b), end=", ")
+        else:
+            print("{:d}{:d}".format(a, b))
