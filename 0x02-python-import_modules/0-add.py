b358bb24a7e2c0ab8ca9806e2775bb084690e48f
sarah mamdouh <sarah.m.alawady@gmail.com>
2023-10-05 14:50:22 -0700

    sara

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """using import function add from add0"""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
