a130114c6241e0ad2ee7d34ddf28f042f64f0620
sarah mamdouh <sarah.m.alawady@gmail.com>
2023-10-08 06:54:04 -0700

    new_string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = " "
+    new = ''
     for i in range(len(my_string)):
         if my_string[i].lower() != 'c':
-            new_string += my_string[i]
-    return new_string
+            new += my_string[i]
+    return new
