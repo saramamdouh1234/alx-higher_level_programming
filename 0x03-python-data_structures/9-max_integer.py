6e68f2874cc9c551dd285e1bd7f080506eaef82a
sarah mamdouh <sarah.m.alawady@gmail.com>
2023-10-09 10:52:42 -0700

    maxx

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    maxN = my_list[0]
+    for i in my_list:
+        if i > maxN:
+            maxN = i
+    return (maxN)
