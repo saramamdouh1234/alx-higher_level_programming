386a7d262a057c7170952a8cb93c97281b70fb7b
sarah mamdouh <sarah.m.alawady@gmail.com>
2024-01-25 11:29:07 -0800

    new

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script to post data (url-encoded) to a server
-curl -s -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -sX POST $1 -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -L
